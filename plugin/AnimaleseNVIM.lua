0bba70821be47f15d74f6aafe2c07ebf69502beb
EZRA-DVLPR <derpisontr@gmail.com>
2025-11-16 13:38:44 -0800

    basic functionality testing for plugin

--- plugin/AnimaleseNVIM.lua
+++ plugin/AnimaleseNVIM.lua
@@ -1,1 +1,6 @@
 print("plugin/example.lua is executed!")
+
+-- check version to see if user can run this program
+if vim.fn.has("nvim-0.7.0") ~= 1 then
+	vim.api.nvim_err_writeln("AnimaleseNVIM requires at least nvim-0.7.0.")
+end
